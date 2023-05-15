146e65897cc2e789be71aeffc71d2c6ea4e13410
Francesco Basile <francesco.basile12@studenti.unina.it>
2023-05-15 18:51:57 +0200

    moved decorators inside test_attack, as they don't belong in the package

--- test_attack.py
+++ test_attack.py
@@ -1,6 +1,38 @@
+import json
+
 from pwn import log
 
-from milkman import local_test
+with open("config.json", "r") as f:  # relative to CWD
+    config: dict = json.load(f)
+
+
+def local_test(exploit):
+    def wrapper(*args, **kwargs):
+        flags = exploit("127.0.0.1")
+        log.info(f"We caught {flags = }")
+
+    return wrapper
+
+
+def nop_test(exploit):
+    def wrapper(*args, **kwargs):
+        nopteam = config["baseip"].format(id=0)
+
+        flags = exploit(nopteam)
+        result = submit_flags(flags)
+        # TODO: add logic to check if everything is working with nopteam
+
+    return wrapper
+
+
+def self_test(exploit):
+    def wrapper(*args, **kwargs):
+        teamip = config["baseip"].format(id=config["team_id"])
+
+        flags = exploit(teamip)
+        log.info(f"We caught {flags = }")
+
+    return wrapper
 
 
 @local_test
@@ -8,9 +40,5 @@ def exploit(target_ip):
     log.info(f"{__name__} against {target_ip}!")
 
 
-def main():
-    exploit()  # type: ignore
-
-
 if __name__ == "__main__":
-    main()
+    exploit()
