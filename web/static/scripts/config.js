13d213d3229df771efe74b20dcdd83bb766a6481
Francesco Basile <francesco.basile12@studenti.unina.it>
2023-05-24 11:37:17 +0200

    I have no idea why this wasn't synced with the repo and getting ignored

---
+++ web/static/scripts/config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2C2C2C",
+        secondary: "#212121",
+        viola: "#821AC2",
+        verde: "#0F932C",
+        rosso: "#B81C1C",
+        light_gray: "#434343",
+      },
+    },
+  },
+};
