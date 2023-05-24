5cf94759bcc21f7fdaa2a2a7c7c6e4d282435b81
Francesco Basile <francesco.basile12@studenti.unina.it>
2023-05-24 12:09:49 +0200

    added new config options

--- app/milkman/processes/GametickManager.py
+++ app/milkman/processes/GametickManager.py
@@ -47,40 +47,65 @@ def waitStart():
     this may be different for other CTFs (besides obviously the URL), so maybe it should be made into an editable module instead.
     """
     while True:  # Wait until the network is open
-        url = "http://10.10.0.1/api/reports/status.json"
         try:
-            status = requests.get(url)
-            if status.json() != {"code": "UNKNOWN", "message": "An error has occurred"}:
+            if isnetworkopen():
                 break
         except Exception:
             pass
         time.sleep(20)
-
-    currentRound = status.json()["currentRound"]
 
 
 def GametickManager():
     tick_n = 0
     log = logger.bind(file="gameloop.log")
     log.debug("GametickManager started")
-    exploits = Exploits()
 
-    waitStart()
-    threads = []
+    if conf["wait_gamestart"]:
+        waitStart()
+
     while True:
+        if conf["use_timer"]:
+            log.info(f"Starting gametick {tick_n}")
+            start = time.time()
+            rungametick(tick_n, log)
+            time.sleep(conf["tick_duration"] - time.time() + start)
+        else:
+            waitGametickNotification(tick_n)
+            log.info(f"Starting gametick {tick_n}")
+            rungametick(tick_n, log)
         tick_n += 1
-        threads.clear()
 
-        log.info(f"Starting gametick {tick_n}")
-        gametickQueue = JoinableQueue()
-        for i, exploit in enumerate(exploits):
-            log.info(f"Starting {exploit.__module__} ({i+1}/{len(exploits)})")
-            threads.append(runExploit(exploit, gametickQueue))
 
-        Thread(
-            target=timeCounterThread,
-            args=(tick_n, gametickQueue, threads),
-            name=f"tick_{tick_n}",
-        ).start()
+def rungametick(tick_n, log):
+    threads = []
+    gametickQueue = JoinableQueue()
+    for i, exploit in enumerate(exploits):
+        log.info(f"Starting {exploit.__module__} ({i+1}/{len(exploits)})")
+        threads.append(runExploit(exploit, gametickQueue))
 
-        time.sleep(conf["tick_duration"])
+    Thread(
+        target=timeCounterThread,
+        args=(tick_n, gametickQueue, threads),
+        name=f"tick_{tick_n}",
+    ).start()
+
+
+def waitGametickNotification(currentRound):
+    url = "http://10.10.0.1/api/reports/status.json"
+    status = requests.get(url)
+    time.sleep(
+        conf["tick_duration"] * 9 / 10
+    )  # wait for most of the gametick; we don't want to harass the gameserver when it's obvious it won't be a new tick in a while
+    while currentRound == status.json()["currentRound"]:
+        time.sleep(
+            conf["tick_duration"] / 20
+        )  # keep asking with more frequency until it's time
+
+
+def isnetworkopen():
+    url = "http://10.10.0.1/api/reports/status.json"
+    status = requests.get(url)
+    if status.json() == {"code": "UNKNOWN", "message": "An error has occurred"}:
+        return False
+    else:
+        return True
