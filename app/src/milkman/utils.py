81605c22d1577fd72a9ac704901ef53fe511a33d
Francesco Basile <francesco.basile12@studenti.unina.it>
2023-06-24 00:15:00 +0200

    reduce requests amount

--- app/src/milkman/utils.py
+++ app/src/milkman/utils.py
@@ -1,10 +1,20 @@
-from requests import get
+from requests import get, ConnectTimeout
+
+_NETOPEN = False
 
 
 def isnetworkopen():
-    url = "http://10.10.0.1/api/reports/status.json"
-    status = get(url)
-    if status.json() == {"code": "UNKNOWN", "message": "An error has occurred"}:
-        return False
-    else:
+    global _NETOPEN
+
+    if _NETOPEN:
         return True
+
+    url = "http://10.10.0.1/api/reports/status.json"
+    try:
+        status = get(url, timeout=5)
+        if status.json() != {"code": "UNKNOWN", "message": "An error has occurred"}:
+            _NETOPEN = True
+    except ConnectTimeout:  # This is mainly for testing
+        _NETOPEN = True
+
+    return _NETOPEN
